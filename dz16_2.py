f04c4cdd08d920909f131c8c71e81c0d6fb4e812
den021912 <usakovdenis440@gmail.com>
2024-07-31 18:15:25 +0400

    Update dz16_2.py

--- dz16_2.py
+++ dz16_2.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI, Path
 from typing import Annotated
 
 app = FastAPI()
+@app.get("/user/{user_id}")
+def user_id(user_id: int = Path(ge=1, le= 100, description = "Enter User ID")) -> dict:
+    return {"message": f"Hello, {user_id} "}
 
-@app.get("/user/{username}/{id}")
-def news(username: Annotated[str, Path(min_length=5, max_length=20, description = "Enter username")]
-         , id: int = Path(ge=18, le= 120, description = "Enter age")) -> dict:
-    return {"message": f"Hello, {username} {id}"}
-# Enter User ID'
+@app.get("/user/{username}/{age}")
+def username_age(username: Annotated[str, Path(min_length=5, max_length=20, description = "Enter username")]
+         , age: int = Path(ge=18, le= 120, description = "Enter age")) -> dict:
+    return {"message": f"Hello, {username} {age}"}
